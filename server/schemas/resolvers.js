cc740cc6e2cadadea96bcffe44b08ebfecfaba86
lapape <lapape07@gmail.com>
2021-06-27 14:58:43 -0600

    added queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,15 +4,6 @@ const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    users: async () => {
-      return User.find().populate("savedBooks");
-    },
-    user: async (parent, { username, _id }, context) => {
-      return User.findOne({
-        $or: [{ _id: context.user._id }, { username: context.user.username }],
-      }).populate("savedBooks");
-    },
-
     me: async (parent, args, context) => {
       if (context.user) {
         return User.findOne({ _id: context.user._id }).populate("savedBooks");
